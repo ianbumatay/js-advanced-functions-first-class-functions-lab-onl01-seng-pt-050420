578b962f83c356ab48ea732f385c58a1364c09c2
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-11-28 18:01:16 -0500

    commit tree 6f36eab52230a8412479908503dd27327f33607d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Code your solution in this file! 
+
+// should return a new array containing the first two drivers from the passed-in array
+
+const returnFirstTwoDrivers = function(drivers){
+    //return [drivers[0], drivers[1]] 
+    return drivers.slice(0,2)
+} 
+
+// should return an array of the last two drivers 
+
+const returnLastTwoDrivers = function(drivers) {
+      return drivers.slice(drivers.length - 2)
+} 
+
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers] 
+
+function createFareMultiplier(multiplier) { 
+    return function(fare) {
+       return fare * multiplier
+    }
+} 
+
+const fareDoubler = createFareMultiplier(2)
+   
+const fareTripler = createFareMultiplier(3) 
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
+  }
